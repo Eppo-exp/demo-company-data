6647c08391fde98144aaf20f1dba3d24d873a9fe
Lukas Goetz-Weiss <lrg130@gmail.com>
2024-01-20 17:58:10 -0700

    early dev

---
+++ packages/data_simulator/data_simulator.py
@@ -0,0 +1,261 @@
+import yaml
+import random
+import pandas as pd
+from datetime import datetime, timedelta
+
+class DataSimulator:
+    def __init__(self, yaml_content):
+        self.config = yaml.safe_load(yaml_content)
+        self.sample_size = self.config.get('sample_size', 1000)
+        self.entity_name = self.config['entity_name']
+        self.experiments = self.config.get('experiments', {})
+        self.dimensions = self.config.get('dimensions', {})
+        self.metrics = self.config.get('metrics')
+
+        self.entity_column = self.entity_name.lower() + '_id'
+
+    def generate_subjects(self):
+        
+        self.subjects = []
+
+        for _ in range(self.sample_size):
+            subject = {}
+            subject[self.entity_column] = _
+            for dim_id, dim_info in self.dimensions.items():
+                subject[dim_id] = random.choices(
+                    population=[var['id'] for var in dim_info['values']],
+                    weights=[var['weight'] for var in dim_info['values']]
+                )[0]
+        
+            self.subjects.append(subject)
+
+    def generate_assignments(self):
+        
+        assignments_data = []
+        for _ in range(self.sample_size):
+
+            for exp_id, exp_info in self.experiments.items():
+                assignment = {}
+
+                start_date = exp_info['start_date']
+                end_date = exp_info['end_date']
+
+                assignment[self.entity_column] = _
+                
+                assignment['date'] = (
+                    start_date + 
+                    timedelta(days=random.randint(0, (end_date - start_date).days))
+                ).strftime('%Y-%m-%d')
+                
+                assignment['experiment'] = exp_id
+
+                assignment['variant'] = random.choices(
+                    population=[var['id'] for var in exp_info['variants']],
+                    weights=[var.get('weight', 1) for var in exp_info['variants']]
+                )[0]
+
+                assignments_data.append(assignment)
+        
+        self.assignments = assignments_data
+    
+    def get_active_experiments(self, date, _):
+        live_experiments = [
+            exp_id
+            for exp_id, exp_info in self.experiments.items() 
+            if exp_info['start_date'] <= date.date() and exp_info['end_date'] >= date.date()
+        ]
+        enrolled_experiments = [
+            a
+            for a in self.assignments
+            if a[self.entity_column] == _
+                and datetime.strptime(a['date'], '%Y-%m-%d') <= date
+                and a['experiment'] in live_experiments
+        ]
+        return enrolled_experiments
+    
+    def compute_model_param(self, conditions, subject, active_experiments):
+        parameter = 0
+        for condition in conditions:
+
+            if 'cond' not in condition:
+                parameter += condition['effect']
+            
+            else:
+                included = True
+                for cond in condition['cond']:
+
+                    if cond['type'] == 'experiment':
+
+                        if len(active_experiments) == 0:
+                            included = False
+                        
+                        else:
+                            exp_var = [
+                                a['variant'] 
+                                for a in active_experiments 
+                                if a['experiment'] == cond['experiment']
+                            ]
+                            
+                            if len(exp_var) == 0:
+                                included = False
+                            
+                            elif exp_var[0] != cond['variant']:
+                                included = False
+                        
+                    if cond['type'] == 'dimension':
+                        if subject[0][cond['id']] != cond['value']:
+                            included = False
+
+                if included:
+                    parameter += condition['effect'] 
+
+        return parameter
+
+
+    def generate_facts(self):
+
+        self.facts = []
+
+        # create date array
+        date_array = pd.date_range(
+            start=self.config['start_date'], 
+            end=self.config['end_date']
+        )
+
+        for _ in range(self.sample_size):
+            _subject = [u for u in self.subjects if u[self.entity_column] == _]
+            for date in date_array:
+
+                active_experiments = self.get_active_experiments(date, _)
+
+                for metric_id, metric_info in self.metrics.items():
+                    if metric_info['model'] == 'poisson_normal':
+                        _lambda = self.compute_model_param(
+                            metric_info['params']['lambda'], 
+                            _subject, 
+                            active_experiments
+                        )
+
+                        self.facts.append(
+                            {
+                                self.entity_column: _,
+                                'date': date,
+                                '_lambda': _lambda
+                            }
+                        )
+
+
+
+                
+
+
+
+        # √for each subject:
+        #    √get dimension values
+        #    √for each day:
+        #       √get experiment variant status 
+        #       √for each metric:
+        #           ...compute model parameters (function of experiment variants, dimension values)
+        #           simulate metric
+        #           append to data if value > 0
+        pass
+
+
+# Sample YAML content
+yaml_content = """
+sample_size: 10
+start_date: 2023-01-01
+end_date: 2023-12-31
+entity_name: User
+dimensions:
+  user_persona:
+    name: User Persona
+    values:
+      - id: designer
+        name: Designer
+        weight: 0.5
+      - id: casual_user
+        name: Casual User
+        weight: 0.5
+  device_type:
+    name: Device Type
+    values:
+      - id: ios
+        name: iOS
+        weight: 0.25
+      - id: android
+        name: Android
+        weight: 0.25
+      - id: web
+        name: Web
+        weight: 0.5
+  device_type:
+    name: Device Type
+    values:
+      - id: ios
+        name: iOS
+        weight: 0.5
+      - id: android
+        name: Android
+        weight: 0.5
+experiments:
+    new_onboarding:
+        name: New Onboarding
+        start_date: 2023-03-01
+        end_date: 2023-05-29
+        variants:
+          - id: control
+            name: Control
+            weight: 0.5
+          - id: test
+            name: Test
+            weight: 0.5
+    search_ranking_algorithm:
+        name: Search Ranking Algorithm
+        start_date: 2023-05-01
+        end_date: 2023-08-29
+        variants:
+          - id: control
+            name: Control
+            weight: 0.33
+          - id: xgboost
+            name: XGBoost
+            weight: 0.33
+          - id: neural_net
+            name: Neural Network
+            weight: 0.34
+metrics:
+  revenue:
+    name: Revenue
+    model: poisson_normal
+    params:
+      lambda:
+        - effect: 1
+        - effect: 0.05
+          cond:
+            - type: experiment
+              experiment: new_onboarding
+              variant: test
+        - effect: -0.15
+          cond:
+            - type: experiment
+              experiment: new_onboarding
+              variant: test
+            - type: dimension
+              id: user_persona
+              value: designer
+"""
+
+
+generator = DataSimulator(yaml_content)
+generator.generate_subjects()
+generator.generate_assignments()
+generator.generate_facts()
+
+print(pd.DataFrame(generator.subjects))
+print(pd.DataFrame(generator.assignments))
+print(pd.DataFrame(generator.facts))
+
+print(
+    pd.DataFrame(generator.facts)['_lambda'].value_counts()
+)
